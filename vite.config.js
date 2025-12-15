9220eb426d9090f8dccf6ffca6f6778c77035e1c
Lorenzo <lorenzopica31@gmail.com>
2025-12-15 18:04:29 +0100

    bug fix: correct path resolution in Vite config

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-// 1. IMPORTA il modulo 'path' di Node.js
 import path from 'path' 
 
 export default defineConfig({
   base: '/Portfolio/',
   plugins: [react()],
-  // 2. AGGIUNGI la configurazione 'resolve'
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),

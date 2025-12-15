8b97947258c95020da7861c964fbba91eb1e7a04
Lorenzo <lorenzopica31@gmail.com>
2025-12-15 12:10:09 +0100

    - Portfolio 100% (Bug Fixed)

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+// 1. IMPORTA il modulo 'path' di Node.js
+import path from 'path' 
+
+export default defineConfig({
+  plugins: [react()],
+  // 2. AGGIUNGI la configurazione 'resolve'
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+})
